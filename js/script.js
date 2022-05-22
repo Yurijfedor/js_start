c44380209f81fe51557a2e0c31fc974474a88863
Yurij  Shaklak <yurijfedor178@gmail.com>
2022-05-22 15:52:44 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-console.log("Hello world !");
+let coloreyes = "";
+let user = "yuriy",
+  username = "Vasja";
+
+user = username;
+
+console.log("35" + -"22");
+console.log("35" * "22");
+
+let usersCounter = 0;
+let newUsers = usersCounter++;
+console.log(newUsers);
+
+console.log((!false && 11) || (18 && !""));
+
+let name = 0;
+console.log(name ?? "Без имени");
+
+let message = 92 > "11" && 58 < 100 ? "true!" : "felse!";
+
+console.log(message);
+
+if (1 === "1") {
+  console.log("true!");
+} else {
+  console.log("felse!");
+}
+
+if (1 == "5") {
+  console.log("true!");
+} else {
+  console.log("felse!");
+}
+
+if (1 === "1") {
+  console.log("true!");
+} else {
+  console.log("felse!");
+}
